e32401847f14611200a10ef0fdad964e6a2828b6
zhengxiangpeng <2013649@mail.nankai.edu.cn>
2023-02-05 19:58:17 +0800

    modified:   common/static/images/price/price-4.jpg 	modified:   frontend/controllers/SiteController.php 	new file:   frontend/views/layouts/gallery_layout.php 	new file:   frontend/views/layouts/price_layout.php

---
+++ frontend/views/layouts/gallery_layout.php
@@ -0,0 +1,179 @@
+<?php
+
+/* @var $this View */
+/* @var $content string */
+
+use yii\helpers\Html;
+use yii\web\View;
+use frontend\assets\AppAsset;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php $this->registerCsrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<header id="header" class="header fixed-top">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+
+        <a href="index.php" class="logo d-flex align-items-center">
+            <img src="<?="../web/statics/assets/img/logo.png"?>" alt="">
+            <span>FlexStart</span>
+        </a>
+
+        <nav id="navbar" class="navbar">
+            <ul>
+                <li><a class="nav-link scrollto" href="index.php#hero">Home</a></li>
+                <li><a class="nav-link scrollto" href="index.php#news">News</a></li>
+                <li><a class="nav-link scrollto active" href="index.php#gallery">Gallery</a></li>
+                <li><a class="nav-link scrollto" href="index.php#comments">Comments</a></li>
+                <li><a class="nav-link scrollto" href="index.php#pricing">Price</a></li>
+                <li><a class="nav-link scrollto" href="index.php#faq">FAQ</a></li>
+                <li><a class="nav-link scrollto" href="index.php#team">Team</a></li>
+                <!-- biandi -->
+                <!-- <li><a class="getstarted scrollto" href="#about">Login</a></li> -->
+                <?php  if (Yii::$app->user->isGuest) { ?>
+
+                    <?= Html::a('Login', ['site/login'],['class'=>'getstarted scrollto']) ?>
+
+                <?php } else { ?>
+
+                    <li class="nav-link scrollto">
+                        <?= Html::beginForm(['/site/logout'], 'post') ?>
+                        <?= Html::submitButton('Logout (' . Yii::$app->user->identity->username . ')',
+                            ['class' => 'btn',
+                                'style' =>
+                                    'display: flex;
+              align-items: center;
+              justify-content: space-between;
+              font-family: "Nunito", sans-serif;
+              font-size: 16px;
+              font-weight: 700;
+              color: #013289;
+              white-space: nowrap;
+
+              background: #4154f1;
+              padding: 8px 20px;
+              margin-left: 30px;
+              border-radius: 4px;
+              color: #fff;']) ?>
+                        <!-- <?= Html::submitButton('Logout (' . Yii::$app->user->identity->username . ')',['class' => 'btn-logout']) ?> -->
+                        <?= Html::endForm() ?>
+                    </li>
+
+                <?php }  ?>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav><!-- .navbar -->
+
+    </div>
+</header><!-- End Header -->
+
+<?= $content ?>
+
+<!-- ======= Footer ======= -->
+<footer id="footer" class="footer">
+
+    <div class="footer-newsletter">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-12 text-center">
+                    <h4>向我们提问</h4>
+                    <p>请在下方留言或提问</p>
+                </div>
+                <div class="col-lg-6">
+                    <?= Html::beginForm('@web/index.php?r=ask%2Fask','post') ?>
+                    <input type="text" class="form-control" name="question" >
+                    <input type="submit" value="提交">
+                    <?= Html::endForm() ?>
+                    <!--<form action="index.php?r=ask%2Fask" method="get">
+                      <input type="email" name="email"><input type="submit" value="Subscribe">
+                    </form>-->
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-top">
+        <div class="container">
+            <div class="row gy-4">
+                <div class="col-lg-5 col-md-12 footer-info">
+                    <a href="index.html" class="logo d-flex align-items-center">
+                        <img src="assets/img/logo.png" alt="">
+                        <span>FlexStart</span>
+                    </a>
+                    <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
+                    <div class="social-links mt-3">
+                        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+                        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-6 footer-links">
+                    <h4>Useful Links</h4>
+                    <ul>
+                        <li><i class="bi bi-chevron-right"></i> <a href="#">Home</a></li>
+                        <li><i class="bi bi-chevron-right"></i> <a href="#">About us</a></li>
+                        <li><i class="bi bi-chevron-right"></i> <a href="#">Services</a></li>
+                        <li><i class="bi bi-chevron-right"></i> <a href="#">Terms of service</a></li>
+                        <li><i class="bi bi-chevron-right"></i> <a href="#">Privacy policy</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-2 col-6 footer-links">
+                    <h4>Our Services</h4>
+                    <ul>
+                        <li><i class="bi bi-chevron-right"></i> <a href="#">Web Design</a></li>
+                        <li><i class="bi bi-chevron-right"></i> <a href="#">Web Development</a></li>
+                        <li><i class="bi bi-chevron-right"></i> <a href="#">Product Management</a></li>
+                        <li><i class="bi bi-chevron-right"></i> <a href="#">Marketing</a></li>
+                        <li><i class="bi bi-chevron-right"></i> <a href="#">Graphic Design</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+                    <h4>Contact Us</h4>
+                    <p>
+                        A108 Adam Street <br>
+                        New York, NY 535022<br>
+                        United States <br><br>
+                        <strong>Phone:</strong> +1 5589 55488 55<br>
+                        <strong>Email:</strong> info@example.com<br>
+                    </p>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="copyright">
+            &copy; Copyright <strong><span>FlexStart</span></strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+            <!-- All the links in the footer should remain intact. -->
+            <!-- You can delete the links only if you purchased the pro version. -->
+            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/flexstart-bootstrap-startup-template/ -->
+            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        </div>
+    </div>
+</footer><!-- End Footer -->
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
