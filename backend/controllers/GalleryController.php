eea49efdae484bd44f90edd0d2d684de51a9af6e
NKULYX <1078346719@qq.com>
2023-02-05 15:41:17 +0800

    update backend user access

--- backend/controllers/GalleryController.php
+++ backend/controllers/GalleryController.php
@@ -6,6 +6,7 @@ use backend\models\RecordUtil;
 use Yii;
 use common\models\Gallery;
 use common\models\GallerySearch;
+use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -21,6 +22,15 @@ class GalleryController extends Controller
     public function behaviors()
     {
         return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => false,
+                        'roles' => ['?'],
+                    ],
+                ],
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
